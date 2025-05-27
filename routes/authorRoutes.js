7136ab424677f175d34c1ed40da20d7adb60e874
tiswan14 <tiswan30@gmail.com>
2025-05-27 23:46:23 +0700

    feat: add routes author

---
+++ routes/authorRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import {
+    getAllAuthors,
+    getAuthorById,
+    createAuthor,
+    updateAuthor,
+    deleteAuthor,
+} from '../controllers/authorController.js'
+
+const router = express.Router()
+
+router.get('/author', getAllAuthors)
+router.get('/author/:id', getAuthorById)
+router.post('/author', createAuthor)
+router.put('/author/:id', updateAuthor)
+router.delete('/author/:id', deleteAuthor)
+
+export default router
